6456d34512312a1718a2a7760a6c11d1b9358b12
Chu <63083746+cli06@users.noreply.github.com>
2021-10-27 12:28:10 -0400

    Add files via upload

---
+++ ps5/ps5-2.py
@@ -0,0 +1,30 @@
+"""
+PHYS-512 PS5
+Q1
+Chuyang Li
+260744689
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+
+def correlation(arr1, arr2):
+    f = arr1
+    g = arr2
+    f = f/f.sum()
+    g = g/g.sum()
+    return np.fft.irfft(np.fft.rfft(f)*np.conj(np.fft.rfft(g)),len(f))
+
+x = np.linspace(-15,15,3001)
+f = np.exp(-0.5*(x-0)**2/1**2)
+f = f/f.sum()
+array = f
+corr = correlation(f, f)
+
+plt.clf()
+plt.plot(x, array, "-b", label="Gaussian")
+plt.plot(x, corr, "-r", label="Correlation")
+plt.legend(loc="upper left")
+plt.show()
+
+
+
